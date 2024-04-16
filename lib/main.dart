26f9151c05ae6036ec9d7b51638f3b477a3f0f4b
Rhys Burman <brhys@umich.edu>
2024-04-16 10:48:39 -0400

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:umich_study/screens/home_screen.dart';
 import 'package:umich_study/location_class/Location.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +23,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-locationBuilder() {
+
+
+locationBuilder() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   List<Location> locations = <Location> [];
   for (var i = 0; i < 100; i++){
     locations.add(const Location());
